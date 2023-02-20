2c2d2963bb59997be04e4d18f57877d3519fc81f
gisioraelvis <gisioraelvis20@gmail.com>
2023-02-20 15:35:46 +0300

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Week7Assignments
+# THEJitu Cohort 9 Training Week 7 Assignments
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.1.5.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'week7-assignments';
+  title = 'Week 7 Assignments';
 }
